7a32ee0b4ca8bb84812ee012557d96767577c36c
Aytaç Kaşoğlu <kasogluaytac44@gmail.com>
2021-10-29 09:18:06 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+#Defined Encoding and decoding function here.
+def caesar(start_text, shift_amount, cipher_direction):
+  end_text = ""
+  if cipher_direction == "decode":
+    shift_amount *= -1
+  for char in start_text:
+     #If the user enters a number/symbol/space, it keeps them when the text is encoded/decoded.
+    if char in alphabet:
+      position = alphabet.index(char)
+      new_position = position + shift_amount
+      end_text += alphabet[new_position]
+    else:
+      end_text += char
+  print(f"Here's the {cipher_direction}d result: {end_text}")
+
+#Import and print the logo from art.py when the program starts.
+from caesarlogo import logo
+print(logo)
+
+#If user want to restart the cipher program. Type 'yes', call the caesar() function again. Otherwise type 'no' for finish program.
+should_end = False
+while not should_end:
+
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  # Even if the user enters a shift that is greater than the number of letters in the alphabet, use (%) modulus. 
+  shift = shift % 26
+
+  caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+  restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+  if restart == "no":
+    should_end = True
+    from replit import clear
+    clear()
+    print("\nThanks to encode/decode with me. See you next time.")
+    
+
+
